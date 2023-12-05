7ce518ab4b0a47d929579e52d798f10a6fed4329
Rafael <hajimemori97@gmail.com>
2023-12-05 20:49:57 -0300

    fixing Web Assembly

--- examples/carrosselV2/model.cpp
+++ examples/carrosselV2/model.cpp
@@ -20,7 +20,7 @@ void Model::computeNormals() {
   }
 
   // Compute face normals
-  for (auto const offset : iter::range(0UL, m_indices.size(), 3UL)) {
+  for (auto const offset : iter::range((unsigned long)0UL, m_indices.size(), 3UL)) {
     // Get face vertices
     auto &a{m_vertices.at(m_indices.at(offset + 0))};
     auto &b{m_vertices.at(m_indices.at(offset + 1))};
